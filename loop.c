dd3f6b9fe1b082ae0ba8550040c4e1617fba1a58
Ananthaselvi <ananthasri44@gmail.com>
2017-06-22 16:07:40 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,s=0;
+	printf("Enter the number");
+	scanf("%d",&n);
+	for(i=0;i<4;i)
+	{
+		s=s+n;
+                
+	
+	}
+	printf("result:%d",s);
+	return 0;
+}
