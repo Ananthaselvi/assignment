39fb40cdf743ab2eb903009e5078695dc26a47df
Ananthaselvi <ananthasri44@gmail.com>
2017-06-23 14:15:30 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -6,9 +6,9 @@ int main()
     scanf("%c",&a);
 
     if( (a>='a' && a<='z') || a>='A' && a<='Z'))
-        printf("%c is an alphabet.",c);
+        printf("%c is an alphabet.",a);
     else
-        printf("%c is not an alphabet.",c);
+        printf("%c is not an alphabet.",a);
 
     return 0;
 }
